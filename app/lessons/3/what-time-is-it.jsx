f1c5e5150f14eeabc4f698a7e0a4abe253075418
nswarr <nswarr@glgroup.com>
2015-10-02 01:05:01 -0400

    Now asking you the time!

--- app/lessons/3/what-time-is-it.jsx
+++ app/lessons/3/what-time-is-it.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
 
+function getRandomNumber(lower, upper){
+  var range = upper - lower
+  var rando = Math.floor(Math.random() * range)
+  return rando + lower;
+}
+
+function generateRandomTime() {
+  var hours = getRandomNumber(1, 13)
+  var minutes = getRandomNumber(1, 60)
+
+  if(minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  return `${hours}:${minutes}`
+}
+
+var question = new Audio('../audio/lessons/3/what-time-is-it.mp3');
+
 const WhatTimeIsIt = React.createClass({
+  getInitialState(){
+    return { time: generateRandomTime() }
+  },
   render () {
     return (
-      <h1>What time is it?</h1>
+      <div>
+        <h1>What time is it?</h1>
+        <p>{this.state.time}</p>
+        <button onClick={this.generateTime}>Generate again</button>
+      </div>
     )
+  },
+  generateTime() {
+    question.play();
+    this.setState({ time: generateRandomTime() })
   }
 })
 
